ad77b588269fbc14f7f80b874446cdc45a5b3fb1
Valerij Ivashchenko <likemusicdev@gmail.com>
2020-10-20 21:24:20 +0300

    Add empty DbHtmlAttributesCleanCommand.

---
+++ src/DbHtmlAttributesCleanCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Likemusic\DbHtmlAttributesCleaner;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DbHtmlAttributesCleanCommand extends Command
+{
+    protected static $defaultName = 'clear';
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ... put here the code to run in your command
+
+        // this method must return an integer number with the "exit status code"
+        // of the command. You can also use these constants to make code more readable
+
+        // return this if there was no problem running the command
+        // (it's equivalent to returning int(0))
+        return Command::SUCCESS;
+
+        // or return this if some error happened during the execution
+        // (it's equivalent to returning int(1))
+        // return Command::FAILURE;
+    }
+}
